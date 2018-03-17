50d63ba2434096b41c4241207f473df9402c7b72
Adahan Yalçınkaya <adahan.yalcinkaya@gmail.com>
2018-03-17 12:53:29 +0300

    initial coding
    There are 2 screens. Menu and the Play Screen. Menu only has play option. Play Screen only has the playerCharacter which can only move up or move down.

---
+++ src/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.states.GameStateManager;
+import com.mygdx.game.states.MenuState;
+
+public class MyGdxGame extends ApplicationAdapter {
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+
+	public static final String TITLE = "Fight or Flight";
+
+	private GameStateManager gsm;
+	private SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		gsm = new GameStateManager();
+		img = new Texture("menuBackground.png");
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
+	}
+
+	@Override
+	public void render () {
+
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
