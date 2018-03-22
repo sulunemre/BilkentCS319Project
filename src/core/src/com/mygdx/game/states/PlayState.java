b5c15ccaa9363d4c93a613e0b9c29dbec5cb3706
Adahan Yalçınkaya <adahan.yalcinkaya@gmail.com>
2018-03-22 18:55:08 +0300

    Moving the Background
    The background moves in Flight stage.

--- src/core/src/com/mygdx/game/states/PlayState.java
+++ src/core/src/com/mygdx/game/states/PlayState.java
@@ -7,13 +7,15 @@ import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.sprites.PlayerCharacter;
 import com.mygdx.game.sprites.Rock;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Vector2;
 
 import java.util.WeakHashMap;
 
 public class PlayState extends State {
     private PlayerCharacter playerCharacter;
     private Texture backgroundImage;
-
+    private Vector2 backgroundPos1, backgroundPos2;
+    private static final int BACKGROUND_Y_OFFSET = -30;
 
     private Array<Rock> rocks;
 
@@ -25,7 +27,8 @@ public class PlayState extends State {
         playerCharacter = new PlayerCharacter(50,100);
         cam.setToOrtho(false, MyGdxGame.WIDTH / 2, MyGdxGame.HEIGHT / 2);
         backgroundImage = new Texture("background1.png");
-
+        backgroundPos1 = new Vector2(cam.position.x - cam.viewportWidth / 2, BACKGROUND_Y_OFFSET);
+        backgroundPos2 = new Vector2((cam.position.x - cam.viewportWidth / 2) + backgroundImage.getWidth(), BACKGROUND_Y_OFFSET);
         rocks = new Array<Rock>();
 
         for(int i =1; i < ROCK_COUNT; i++){
@@ -50,6 +53,7 @@ public class PlayState extends State {
     @Override
     public void update(float dt) {
         handleInput();
+        updateBackground();
         playerCharacter.update(dt);
         cam.position.x = playerCharacter.getPosition().x + 80;
 
@@ -69,7 +73,9 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(backgroundImage, cam.position.x - (cam.viewportWidth /2 ), 0);
+       // sb.draw(backgroundImage, cam.position.x - (cam.viewportWidth /2 ), 0);
+        sb.draw(backgroundImage, backgroundPos1.x, backgroundPos1.y);
+        sb.draw(backgroundImage, backgroundPos2.x, backgroundPos2.y);
         sb.draw(playerCharacter.getPlayerCharacter(), playerCharacter.getPosition().x, playerCharacter.getPosition().y );
      //   sb.draw(rock.getRock1(), rock.getRock1Pos().x, rock.getRock1Pos().y);
         for(Rock rock: rocks){
@@ -81,6 +87,16 @@ public class PlayState extends State {
 
     @Override
     public void dispose() {
+
+    }
+    private void updateBackground(){
+        if(cam.position.x - (cam.viewportWidth / 2) > backgroundPos1.x + backgroundImage.getWidth())
+           backgroundPos1.add(backgroundImage.getWidth() * 2, 0);
+        if(cam.position.x - (cam.viewportWidth / 2) > backgroundPos2.x + backgroundImage.getWidth())
+            backgroundPos2.add(backgroundImage.getWidth() * 2, 0);
+
+
+
 
     }
 }
