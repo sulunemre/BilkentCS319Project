3feadc41ad12f0edbe59cc3c24736954121ef4f1
Emre Sülün <emresulun93@gmail.com>
2018-05-01 16:11:15 +0300

    State transition problem fixed

--- src/core/src/com/mygdx/game/stateControllers/FightStateController.java
+++ src/core/src/com/mygdx/game/stateControllers/FightStateController.java
@@ -3,6 +3,7 @@ package com.mygdx.game.stateControllers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -12,6 +13,8 @@ import com.mygdx.game.sprites.*;
 import com.mygdx.game.sprites.enemies.Enemy;
 import com.mygdx.game.sprites.enemies.EnemyFactory;
 import com.mygdx.game.sprites.enemies.Grunt;
+import com.mygdx.game.states.PlayState;
+
 
 
 public class FightStateController extends AbstractStateController {
@@ -21,18 +24,21 @@ public class FightStateController extends AbstractStateController {
     private Array<Enemy> enemyArray;
     private int wave;
     private boolean waveCleared;
+    private Texture backgroundImage;
 
     private Array<Rock> rocks;
     private Array<RockMoving> rocksM;
     private static final int ROCK_SPACING = 125;
     private static final int ROCK_COUNT = 3;
+    private static final int BACKGROUND_Y_OFFSET = -30;
+    private Vector2 backgroundPos1, backgroundPos2;
 
     public FightStateController() {
         gameWorld = GameWorld.getInstance();
         gameManager = GameManager.getInstance();
         playerCharacter = gameWorld.getPlayerCharacter();
         enemyArray = gameWorld.getEnemyArray();
-
+        backgroundImage = new Texture("background1.png");
         wave = 1;
         waveCleared = true;
 
@@ -42,10 +48,11 @@ public class FightStateController extends AbstractStateController {
 
             rocks.add(new Rock(i * (ROCK_SPACING + Rock.TUBE_WIDTH), 5));
         }
-//        backgroundPos1 = new Vector2(cam.position.x - cam.viewportWidth / 2, BACKGROUND_Y_OFFSET);
-//        backgroundPos2 = new Vector2((cam.position.x - cam.viewportWidth / 2) + backgroundImage.getWidth(), BACKGROUND_Y_OFFSET);
-//        gameWorld.setBackgroundPos1(backgroundPos1);
-//        gameWorld.setBackgroundPos2(backgroundPos2);
+
+        backgroundPos1 = new Vector2(cam.position.x - cam.viewportWidth / 2, BACKGROUND_Y_OFFSET);
+        backgroundPos2 = new Vector2((cam.position.x - cam.viewportWidth / 2) + backgroundImage.getWidth(), BACKGROUND_Y_OFFSET);
+        gameWorld.setBackgroundPos1(backgroundPos1);
+        gameWorld.setBackgroundPos2(backgroundPos2);
 
         playerCharacter.setDirection(new Vector2(0,0)); // At the beginning of the state, player stops
     }
@@ -65,6 +72,9 @@ public class FightStateController extends AbstractStateController {
         }
         if(input.isKeyPressed(Input.Keys.D)){
             playerCharacter.moveRight();
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            gameStateManager.set(new PlayState());
         }
 
         if(input.justTouched()){
@@ -89,7 +99,7 @@ public class FightStateController extends AbstractStateController {
     public void update(float dt) {
 
         handleInput();
-      //  updateBackground();
+        updateBackground();
         gameWorld.updateAll(dt);
 
         cam.position.x = playerCharacter.getPosition().x + 80;
@@ -101,36 +111,7 @@ public class FightStateController extends AbstractStateController {
 
         cam.update();
 
-//        for(int i=0; i<enemyArray.size; i++)
-//        {
-//            Enemy currentEnemy = enemyArray.get(i);
-//
-//
-//
-//            for(int j=0; j<enemyArray.size; j++) {
-//                Enemy secondEnemy = enemyArray.get(j);
-//
-//                if (j != i)
-//                {
-//                    if(currentEnemy.collides(secondEnemy.getBounds()))
-//                    {
-//                        System.out.println("collide var i: " + i + " j: " + j);
-//                        currentEnemy.setPosition(currentEnemy.getPosition());
-//                    }
-//                    //    if(currentEnemy.collides(secondEnemy.getBounds()) && playerCharacter.collides(currentEnemy.getBounds()))
-//                    // {
-//                    //     System.out.println("collide var i: " + i + " j: " + j);
-//                    //       currentEnemy.setPosition(currentEnemy.getPosition().add(-0.4f,-0.4f));
-//                    //   }
-//                    else
-//                    {
-//                        currentEnemy.chase(playerCharacter.getPosition().x, playerCharacter.getPosition().y);
-//                    }
-//
-//                }
-//                System.out.println("enemy direction x: " + currentEnemy.getDirection().x + " y: " + currentEnemy.getDirection().y);
-//            }
-//        }
+
 
         if(waveCleared) {
             sendNewWave();
@@ -138,17 +119,21 @@ public class FightStateController extends AbstractStateController {
         for(Enemy enemy : enemyArray){
             enemy.chase( gameWorld.getPlayerCharacter().getPosition().x , gameWorld.getPlayerCharacter().getPosition().y);
         }
+        if (gameWorld.getPlayerCharacter().getPosition().x < 0)
+            gameWorld.getPlayerCharacter().getPosition().x = 0;
+        if (gameWorld.getPlayerCharacter().getPosition().x > 500)
+            gameWorld.getPlayerCharacter().getPosition().x = 500;
         collisionTrigger();
         //gameWorld.getEnemyArray().get(1).chase(gameWorld.getPlayerCharacter().getPosition().x, gameWorld.getPlayerCharacter().getPosition().y );
     }
 
-//    private void updateBackground(){
-//        if(cam.position.x - (cam.viewportWidth / 2) > backgroundPos1.x + backgroundImage.getWidth())
-//            backgroundPos1.add(gameWorld.backgroundImage.getWidth() * 2, 0);
-//        if(cam.position.x - (cam.viewportWidth / 2) > backgroundPos2.x + backgroundImage.getWidth())
-//            backgroundPos2.add(backgroundImage.getWidth() * 2, 0);
-//
-//    }
+   private void updateBackground(){
+        if(cam.position.x - (cam.viewportWidth / 2) > backgroundPos1.x + backgroundImage.getWidth())
+            backgroundPos1.add(backgroundImage.getWidth() * 2, 0);
+        if(cam.position.x - (cam.viewportWidth / 2) > backgroundPos2.x + backgroundImage.getWidth())
+            backgroundPos2.add(backgroundImage.getWidth() * 2, 0);
+
+    }
 
     private void sendNewWave() {
 
